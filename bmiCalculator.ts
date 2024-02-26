e15c614869c3a31037f025dc44eacbfd4f2402ee
Chen Xu <cxu@MacBook-Pro.local>
2024-02-26 08:47:21 +0200

    Refactor BMI calculator to separate CLI functionality

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,19 +1,4 @@
-import { isNotNumber } from "./utils";
-
-const parseBmiArguments = (args: string[]): { height: number, weight: number } => {
-    if (args.length !== 4) throw new Error('Usage: npm run calculateBmi <height>(cm) <weight>(kg)');
-
-    const height = Number(args[2]);
-    const weight = Number(args[3]);
-
-    if (isNotNumber(height) || isNotNumber(weight)) {
-        throw new Error('Provided values must be numbers!');
-    }
-
-    return { height, weight };
-};
-
-function calculateBmi(height: number, weight: number): string {
+export const calculateBmi = (height: number, weight: number): string => {
     const heightInMeters = height / 100;
 
     const bmi = weight / (heightInMeters * heightInMeters);
@@ -27,17 +12,4 @@ function calculateBmi(height: number, weight: number): string {
     } else {
         return 'Obese';
     }
-}
-
-try {
-    const { height, weight } = parseBmiArguments(process.argv);
-    console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-    let errorMessage = 'Error: ';
-    if (error instanceof Error) {
-        errorMessage += error.message;
-    }
-    console.log(errorMessage);
-}
-
-export { calculateBmi };
+};
