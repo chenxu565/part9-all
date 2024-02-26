03d47d51c6ccd94885cc297498fe0ac9a951544d
Chen Xu <cxu@MacBook-Pro.local>
2024-02-26 08:52:47 +0200

    Refactor Exercise calculator to separate CLI functionality

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,5 +1,3 @@
-import { isNotNumber } from "./utils";
-
 interface ExerciseResult {
     periodLength: number;
     trainingDays: number;
@@ -9,19 +7,6 @@ interface ExerciseResult {
     target: number;
     average: number;
 }
-
-const parseExerciseArguments = (args: string[]): { target: number, dailyHours: number[] } => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-
-    const target = Number(args[2]);
-    const dailyHours = args.slice(3).map(arg => Number(arg));
-
-    if (isNotNumber(target) || dailyHours.some(isNotNumber)) {
-        throw new Error('Provided values must be numbers!');
-    }
-
-    return { target, dailyHours };
-};
 
 function calculateExercises(dailyHours: number[], target: number): ExerciseResult {
     const periodLength = dailyHours.length;
@@ -55,13 +40,4 @@ function calculateExercises(dailyHours: number[], target: number): ExerciseResul
     };
 }
 
-try {
-    const { target, dailyHours } = parseExerciseArguments(process.argv);
-    console.log(calculateExercises(dailyHours, target));
-} catch (error: unknown) {
-    let errorMessage = 'Error: ';
-    if (error instanceof Error) {
-        errorMessage += error.message;
-    }
-    console.log(errorMessage);
-}
+export { calculateExercises };
